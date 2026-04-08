340375d0c7ae8ef61c07277966ff6b063179ae89
Divyashree V Chalavadhi <148611914+13divyashree@users.noreply.github.com>
2026-04-08 12:49:49 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,60 +1,52 @@
 import pandas as pd
+import numpy as np
 import streamlit as st
 from sklearn.cluster import KMeans
-from kneed import KneeLocator
+from kneed import kneeLocator
 from sklearn.preprocessing import LabelEncoder
 from sklearn.metrics import silhouette_score
 
-st.set_page_config(page_icon='🛃', page_title = 'Customer Segmentation', layout='wide')
 
-#load the data
-file = st.file_uploader(" ",type=['CSV'])
-df = None
+st.set_page_config(page_icon="👥",page_title="Customer Segmentation",layout="wide")
+ 
+
+file=st.file_uploader(" ",type='csv')
+df=None
 if file:
     df=pd.read_csv(file)
 
+
 with st.sidebar:
-    st.title('Customer Segmentation')
+    st.title("Customer Segmentation")
     if df is not None:
-        features = st.multiselect('Select Features:',options=df.columns,
-                                  default=['Annual Income (k$)', 'Spending Score (1-100)'])
-        df = df.loc[:,features]
+        features=st.multiselect("SELECT FEATURE",options=df.columns,default=["Annual Income (k$)","Spending Score (1-100)"])
+        df=df.loc[:features]
 
 def preprocessing(df):
-    #encoding
-    encoder = LabelEncoder()
+    encoder=LabelEncoder()
     for col in df.columns:
         if df[col].dtype==object:
             df[col]=encoder.fit_transform(df[col])
 
 def elbow():
     out=[]
-    k_values = range(1,11)
+    k_values=range(1,11)
     for i in k_values:
-        model=KMeans(n_clusters=i)
+        model=KMeans(n_clustres=i)
         model.fit(df)
         out.append(model.inertia_)
-    KL=KneeLocator(k_values, out, curve='convex', direction='decreasing')
-    df1 = pd.DataFrame({'k_values':k_values,'inertia':out})
-    st.subheader('Elbow Curve')
-    fig = st.line_chart(data=df1, x="k_values", y="inertia")
+    KL=KneeLocator(k_values,out,curve="convex",direction="decreasing")
+    df1=pd.DataFrame({"k_values":k_values,"inertia":out})
+    st.header("ELBOW CURVE")
+    fig=st.line_chart(data=df1,x="k_values",y="inrtia")
     return KL.elbow
 
+
+
 if df is not None:
-    st.subheader('Sample Data')
+    st.subheader("Sample data")
     st.write(df.sample(10))
     preprocessing(df)
 
-    #optimised k value
-    k = elbow()
 
-    #model training
-    model = KMeans(n_clusters=k)
-    model.fit(df)
-    labels=model.labels_
-    df['clusters']=labels
-
-    #visualisation of clusters
-    st.subheader('Clustered Data')
-    st.scatter_chart(data=df, x='Annual Income (k$)', y='Spending Score (1-100)', color='clusters')
 
